77bbe7bfcac26b8a0162263f017d6a91a0fcc0f9
Kirill Guryev <gurev.matvei@gmail.com>
2021-09-28 16:23:53 +0300

    Добавил методы  для выпонленения запросов на получения выбранного эпизода и выбранного персонажа

--- src/services/DataServices.js
+++ src/services/DataServices.js
@@ -6,6 +6,15 @@ class DataService {
   getAll(page) {
     return axios.get(`${API_URL}/character/?page=${page}`)
   }
+  getCurrentCharacter(characterId) {
+    return axios.get(`${API_URL}/character/${characterId}`)
+  }
+  getCurrentEpisode(episodeId) {
+    return axios.get(`${API_URL}/episode/${episodeId}`)
+  }
+  getCharacter(url) {
+    return axios.get(`${url}`)
+  }
 }
 
 export default new DataService()
