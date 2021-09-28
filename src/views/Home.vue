9c8a10c6e0ab1fc16d4bacc7ebe4d7e597b0bce6
Kirill Guryev <gurev.matvei@gmail.com>
2021-09-28 16:20:18 +0300

    Добавил router-link для переходу на страницы episode и character

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,46 @@
 <template>
-  <div style="text-align: center; margin: 2rem 0">
+
+  <div  style="text-align: center; margin: 2rem 0">
     <h1 class="title">Rick and Morty</h1>
   </div>
-
-  <table>
-    <thead>
+     <div class="filter-wrapper">
+        <div class="search-container">
+          <div class="input-wrapper">
+          <input class="input" type="text">
+          </div>
+          <select name="" id="">asdsa</select>
+      </div>
+    </div>
+  <table >
+    <!-- <thead>
       <tr>
         <td>Имя</td>
         <td>Разновидность</td>
         <td>Фото</td>
         <td>Эпизоды</td>
       </tr>
-    </thead>
-    <tbody>
+      
+    </thead> -->
+ 
+     
+    <tbody >
       <tr v-for="p in humans" :key="p.id">
-        <td>{{ p.name }}</td>
+        <td>
+          <router-link :to="{ name: 'Character', params: { charId: p.id } }">{{
+            p.name
+          }}</router-link>
+        </td>
         <td>{{ p.status }}</td>
         <td><img :src="p.image" alt="" /></td>
         <td>
-          <a
+          <router-link
             href=""
+            to: :to="{ name: 'Episode', params: { episodeId: episode } }"
             v-for="(episode, index) in p.episode
               .map(el => +el.match(/(\d+)/)[0])
               .slice(0, 5)"
             :key="index"
-            >{{ `${episode} ` }}</a
+            >{{ `${episode} ` }}</router-link
           >
         </td>
       </tr>
@@ -35,9 +51,18 @@
 <script>
 import DataService from '../services/DataServices'
 import { mapState } from 'vuex'
+
+
+
 export default {
+ 
   name: 'Home',
-  components: {},
+
+  data() {
+    return {
+      isLoading: true
+    }
+  },
   mounted() {
     this.getHuman()
   },
@@ -53,3 +78,22 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.filter-wrapper {
+  width: 100%;
+  background: hotpink;
+  height: 60px;
+  display: flex;
+  align-items: center;
+ 
+}
+.search-container{
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  
+}
+
+
+</style>
