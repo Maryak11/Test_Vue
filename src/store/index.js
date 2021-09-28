a8b1f3538b416c25e5cd526eef50482116e694bc
Kirill Guryev <gurev.matvei@gmail.com>
2021-09-28 16:25:21 +0300

    Добавил в стейт текущий эпизод

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    humans: []
+    humans: [],
+    currentEpisode: null
   },
   mutations: {
     setHumans(state, payload) {
       state.humans.push(...payload)
+    },
+    setCurrentEpisode(state, payload) {
+      state.currentEpisode = payload
     }
   },
   actions: {
     setHumans(context, payload) {
       context.commit('setHumans', payload)
+    },
+    setCurrentEpisode(context, payload) {
+      context.commit('setCurrentEpisode', payload)
     }
   },
   modules: {}
